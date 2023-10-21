d64168af03615de2cff042072888375720736eff
Wendell Leão <leaowendell@outlook.com>
2023-10-21 01:49:17 -0300

    Implement Character flip

--- Assets/_Project/Scripts/Gameplay/Playing/CharacterMovement.cs
+++ Assets/_Project/Scripts/Gameplay/Playing/CharacterMovement.cs
@@ -12,6 +12,7 @@ namespace Bloodlust.Gameplay.Playing
         
         private PlayerControls _playerControls;
         private Vector2 _move;
+        private bool _isFacingRight = true;
 
         public void Begin(PlayerControls playerControls)
         {
@@ -24,11 +25,28 @@ namespace Bloodlust.Gameplay.Playing
         public void Tick(float deltaTime)
         {
             _move = GetNormalizedMove();
+
+            HandleFlip();
         }
-        
+
         public void FixedTick(float fixedDeltaTime)
         {
             _rigidbody.velocity = _move * (fixedDeltaTime * _moveSpeed);
+        }
+
+        private void HandleFlip()
+        {
+            if (_isFacingRight && _move.x <= -1f || !_isFacingRight && _move.x >= 1f)
+            {
+                Transform myTransform = transform;
+                Vector3 localScale = myTransform.localScale;
+
+                float localScaleX = localScale.x;
+                localScaleX *= -1f;
+                myTransform.localScale = new Vector3(localScaleX, localScale.y, localScale.z);
+
+                _isFacingRight = !_isFacingRight;
+            }
         }
 
         private Vector2 GetNormalizedMove()
